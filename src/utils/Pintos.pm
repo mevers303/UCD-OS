2087f4d166a53aa5b55c1e1f29e62f6e8bc41c06
Scott Meyer <Scott.R.Meyer@gmail.com>
2016-03-09 13:03:24 -0700

    Updated for path /home/git-repos/UCD-OS

--- src/utils/Pintos.pm
+++ src/utils/Pintos.pm
@@ -359,7 +359,7 @@ sub cyl_sectors {
 # Makes sure that the loader is a reasonable size.
 sub read_loader {
     my ($name) = @_;
-    $name = find_file ("/home/penguin/git-repos/pintos-anon/src/threads/build/loader.bin") if !defined $name;
+    $name = find_file ("/home/git-repos/UCD-OS/src/threads/build/loader.bin") if !defined $name;
     die "Cannot find loader\n" if !defined $name;
 
     my ($handle);
